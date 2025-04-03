683493728ba8cf289805bdfbe430ac292907dacf
Arthur Santos <arthurfernandes1993@gmail.com>
2025-04-03 20:02:50 -0300

    Fix lang on data-handler.js

--- scripts/data-handler.js
+++ scripts/data-handler.js
@@ -1,26 +1,32 @@
 import data from "./components/resume.json" with { type: "json" };
 
 window.addEventListener("load", () => {
-  const userLocale =
-    navigator.languages && navigator.languages.length
-      ? navigator.languages[0]
-      : navigator.language;
-  let lang = userLocale ? userLocale.substring(0, 2) : "en";
+  const getLang = () => {
+    let language = sessionStorage.getItem("language") ? sessionStorage.getItem("language") : "en-US";
+    if (sessionStorage.getItem("language") == null) {
+      const userLocale =
+      navigator.languages && navigator.languages.length
+        ? navigator.languages[0]
+        : navigator.language;
+      
+      language = userLocale;
+      sessionStorage.setItem("language", language ? language.substring(0, 2) : language);
+    }
+
+    document.querySelector("html").lang = language;
+    return sessionStorage.getItem("language").substring(0, 2);
+  }
   
+
+  let lang = getLang();
+
   let page = document.querySelector(".page");
-
-
-  /**
-   * Change approach to add the content information and not to add new elements
-   */
-
 
   const handleSection = (section) => {
     if ( section.title ) {
       page.querySelector(`#${section.title.placeholder}`).textContent = section.title.content[lang] ? section.title.content[lang] : section.title.content;
     }
     if ( section.image ) {
-      console.log(section.image);
       page.querySelector(`#${section.image.placeholder}`).alt = section.image.alt[lang] ? section.image.alt[lang] : section.image.alt;
     }
     if ( section.items ) {
@@ -32,7 +38,7 @@ window.addEventListener("load", () => {
 
   // Handle menu items
   handleSection(data.menu);
-  handleSection(data.profile);
+  handleSection(data.about);
 
 
 
